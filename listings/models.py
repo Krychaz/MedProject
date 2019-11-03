df4661bad5d1f688c4b52fa15d2eea6e71e5708b
Krystian <krychaz@protonmail.com>
2019-11-03 12:15:53 +0000

    db schema created

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
+from prof.models import Doctor
 
-# Create your models here.
+class listing(models.Model):
+  doctor = models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
+  name = models.CharField(max_length=200)
+  age = models.IntegerField(default=0)
+  gender = models.CharField(max_length=20)
+  ethnicity = models.CharField(max_length=50)
+  height = models.IntegerField(default=0)
+  weight = models.IntegerField(default=0)
+  PAL = models.IntegerField(default=0)
+  blood_p = models.IntegerField(default=0)
+  AIPW = models.IntegerField(default=0)
+  SIPW = models.IntegerField(default=0)
+  smoker = models.BooleanField()
+  hearthbeat = models.BooleanField()
+  date = models.DateField(default=datetime.now, blank=true)
+def __str__(self):
+    return self.name
